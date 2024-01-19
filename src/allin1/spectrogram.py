44c11c617147f7016b5b191cd28020f11772e799
musikamelabs <123078605+musikamelabs@users.noreply.github.com>
2024-01-19 10:14:27 +0100

    Update spectrogram.py
    Spectrogram perf test

--- src/allin1/spectrogram.py
+++ src/allin1/spectrogram.py
@@ -1,3 +1,4 @@
+"""
 import numpy as np
 from pathlib import Path
 from typing import List, Tuple
@@ -74,6 +75,81 @@ def _extract_spectrogram(args: Tuple[Path, Path, SequentialProcessor]):
   spec_drums = processor(sig_drums)
   spec_others = processor(sig_other)
   spec_vocals = processor(sig_vocals)
+"""  
+
+# ### ALTERMATIVE VERSION TEST
+import numpy as np
+from pathlib import Path
+from typing import List, Tuple
+from tqdm import tqdm
+from multiprocessing import Pool
+from madmom.audio.signal import FramedSignalProcessor, Signal, AudioReaderProcessor
+from madmom.audio.stft import ShortTimeFourierTransformProcessor
+from madmom.processors import SequentialProcessor
+from madmom.audio.spectrogram import FilteredSpectrogramProcessor, LogarithmicSpectrogramProcessor
+
+
+def extract_spectrograms(demix_paths: List[Path], spec_dir: Path, multiprocess: bool = True):
+  todos = []
+  spec_paths = []
+  for src in demix_paths:
+    dst = spec_dir / f'{src.stem}.npy'
+    spec_paths.append(dst)
+    if dst.is_file():
+      continue
+    todos.append((src, dst))
+
+  existing = len(spec_paths) - len(todos)
+  print(f'=> Found {existing} spectrograms already extracted, {len(todos)} to extract.')
+
+  if todos:
+    # Define a pre-processing chain, which is copied from madmom.
+    frames = FramedSignalProcessor(
+      frame_size=2048,
+      fps=int(44100 / 441)
+    )
+    stft = ShortTimeFourierTransformProcessor()  # caching FFT window
+    filt = FilteredSpectrogramProcessor(
+      num_bands=12,
+      fmin=30,
+      fmax=17000,
+      norm_filters=True
+    )
+    spec = LogarithmicSpectrogramProcessor(mul=1, add=1)
+    audio_reader = AudioReaderProcessor()
+    processor = SequentialProcessor([audio_reader, frames, stft, filt, spec])
+
+    # Process all tracks using multiprocessing.
+    if multiprocess:
+      pool = Pool()
+      map_fn = pool.imap
+    else:
+      pool = None
+      map_fn = map
+
+    iterator = map_fn(_extract_spectrogram, [
+      (src, dst, processor)
+      for src, dst in todos
+    ])
+    for _ in tqdm(iterator, total=len(todos), desc='Extracting spectrograms'):
+      pass
+
+    if pool:
+      pool.close()
+      pool.join()
+
+  return spec_paths
+
+
+def _extract_spectrogram(args: Tuple[Path, Path, SequentialProcessor]):
+  src, dst, processor = args
+
+  dst.parent.mkdir(parents=True, exist_ok=True)
+
+  audio_source = AudioDataSource(str(src), num_channels=4, sample_rate=44100)
+  spec = processor(audio_source)
+
+  np.save(str(dst), spec)
 
   spec = np.stack([spec_bass, spec_drums, spec_others, spec_vocals])  # instruments, frames, bins
 
